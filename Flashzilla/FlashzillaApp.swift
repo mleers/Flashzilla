58ff7d5167f0d07f6699f1f79186d441d0b850cf
mleers <mleers4@gmail.com>
2023-01-12 12:53:02 -0800

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Mitch on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
